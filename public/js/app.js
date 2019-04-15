ee934bbc4d2fe5d2c065b4aeb1fc9cf72ac8436e
Bleau Moores <bdmoores@lakeheadu.ca>
2019-04-15 02:45:39 -0400

    Woot bd api is wired!

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,6 @@
 console.log('Client side javascript file is loaded.');
 
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log(data)
 
-    })
-})
 
 // fetch('/stats', {method:'GET'}).then((response) => {
 //   response.json().then((data) => {
@@ -18,7 +13,66 @@ fetch('http://puzzle.mead.io/puzzle').then((response) => {
 //   response.json().then((data) => {
 //     _class = data
 //       console.log(data)
-//       console.log('arcane:'+data.arcane)
 //   })
 // })
 
+const charForm =document.querySelector('form')
+const classSelector = document.querySelector("#charClass")
+const raceSelector = document.querySelector("#charRace")
+const displayStats = document.querySelector("#stats")
+const raceAttributes = [2, 2, 2, 2, 2, 2, 2]
+const classAttributes = [2, -1, -1, -1, 2, 2, 4]
+
+
+
+charForm.addEventListener('change',(e) => {
+  e.preventDefault()
+  const selectedClass = classSelector.value
+  const selectedRace = raceSelector.value
+
+  fetch('/stats/'+selectedClass, {method:'GET'}).then((response) => {
+    response.json().then((data) => {
+      _class = data
+      classAttributes[0] = data.arcane
+      classAttributes[1] = data.martial
+      classAttributes[2] = data.deception
+      classAttributes[3] = data.devotion
+      classAttributes[4] = data.hand
+      classAttributes[5] = data.gold
+      classAttributes[6] = data.hp
+      console.log(data)
+      console.log(classAttributes)
+    })
+  })
+
+  fetch('/stats/'+selectedRace, {method:'GET'}).then((response) => {
+    response.json().then((data) => {
+      raceAttributes[0] = data.arcane
+      raceAttributes[1] = data.martial
+      raceAttributes[2] = data.deception
+      raceAttributes[3] = data.devotion
+      raceAttributes[4] = data.hand
+      raceAttributes[5] = data.gold
+      raceAttributes[6] = data.hp
+      console.log(data)
+      console.log(raceAttributes)
+    })
+  })
+
+})
+
+charForm.addEventListener('submit',(e)=>{
+  e.preventDefault()
+
+  const totalAttributes=[] 
+  for(i=0;i<7;i++){
+    totalAttributes[i] = raceAttributes[i]+classAttributes[i]
+  }
+  console.log(totalAttributes)
+  var x = displayStats.rows
+  var y = x[0].cells
+  console.log(classAttributes)
+  for (i=0; i<y.length;i++){
+    y[i].innerHTML=totalAttributes[i]
+  }
+})
